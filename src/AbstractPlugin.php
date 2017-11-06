750f25b6f2e62bb0e410bdccf0323edd3436a99b
Igor Popravka <igor.popravka@dukascopy.com>
2017-11-06 11:14:03 +0200

    WPPBUILDER-01

---
+++ src/AbstractPlugin.php
@@ -0,0 +1,13 @@
+<?php
+namespace WDIP\WPPBuilder;
+
+use WDIP\WPPBuilder\Interfaces\PluginInterface;
+
+/**
+ * @author: igor.popravka
+ * Date: 06.11.2017
+ * Time: 11:09
+ */
+abstract class AbstractPlugin implements PluginInterface {
+
+}
---
+++ src/Interfaces/PluginInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace WDIP\WPPBuilder\Interfaces;
+
+/**
+ * @author: igor.popravka
+ * Date: 06.11.2017
+ * Time: 11:12
+ */
+interface PluginInterface {
+
+}
